5f768c2044d0827739c53ffc42a33d4ed6482586
Ethan Lee <flibitijibibo@gmail.com>
2024-10-29 09:28:56 -0400

    Hello CI!

---
+++ build-fnalibs-apple.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO: Draw the rest of the owl
+
+mkdir fnalibs-apple
+cp fnalibs-apple.README fnalibs-apple/README.txt
---
+++ build-fnalibs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO: Draw the rest of the owl
+
+mkdir fnalibs
+cp fnalibs.README fnalibs/README.txt
