d02dfc446b4b091ab37295112f838b45e9e1814b
Ethan Lee <flibitijibibo@gmail.com>
2024-10-31 09:29:59 -0400

    Initial build-fnalibs.sh

--- build-fnalibs.sh
+++ build-fnalibs.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 
-# TODO: Draw the rest of the owl
+set -ex
 
-mkdir fnalibs
+workflow_id=$(gh -R libsdl-org/SDL run list -b main --json databaseId --jq '.[0].databaseId')
+gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-slrsniper'
+gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-VC-x86'
+gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-VC-x64'
+
+workflow_id=$(gh -R FNA-XNA/FAudio run list -b master --json databaseId --jq '.[0].databaseId')
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-lib64'
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-x86'
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-x64'
+
+workflow_id=$(gh -R FNA-XNA/FNA3D run list -b master --json databaseId --jq '.[0].databaseId')
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-lib64'
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-x86'
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-x64'
+
+workflow_id=$(gh -R FNA-XNA/Theorafile run list -b master --json databaseId --jq '.[0].databaseId')
+gh -R FNA-XNA/Theorafile run download ${workflow_id} -p 'Theorafile-lib64'
+gh -R FNA-XNA/Theorafile run download ${workflow_id} -p 'Theorafile-x86'
+gh -R FNA-XNA/Theorafile run download ${workflow_id} -p 'Theorafile-x64'
+
+mkdir -p fnalibs
 cp fnalibs.README fnalibs/README.txt
+
+mkdir -p fnalibs/lib64
+tar xvfz SDL-slrsniper/dist/SDL3-3.1.5-Linux.tar.gz SDL3-3.1.5-Linux/lib/libSDL3.so.0.1.5
+mv SDL3-3.1.5-Linux/lib/libSDL3.so.0.1.5 fnalibs/lib64/libSDL3.so.0
+chmod +x fnalibs/lib64/libSDL3.so.0
+strip -S fnalibs/lib64/libSDL3.so.0
+mv FAudio-SDL3-lib64/libFAudio.so.0 fnalibs/lib64/
+mv FNA3D-SDL3-lib64/libFNA3D.so.0 fnalibs/lib64/
+mv Theorafile-lib64/libtheorafile.so fnalibs/lib64/
+
+mkdir -p fnalibs/x86
+unzip SDL-VC-x86/dist/SDL3-3.1.5-Windows-VC.zip SDL3-3.1.5-Windows-VC/bin/SDL3.dll
+mv SDL3-3.1.5-Windows-VC/bin/SDL3.dll fnalibs/x86/
+mv FAudio-SDL3-x86/FAudio.dll fnalibs/x86/
+mv FNA3D-SDL3-x86/FNA3D.dll fnalibs/x86/
+mv Theorafile-x86/libtheorafile.dll fnalibs/x86/
+
+mkdir -p fnalibs/x64
+unzip SDL-VC-x64/dist/SDL3-3.1.5-Windows-VC.zip SDL3-3.1.5-Windows-VC/bin/SDL3.dll
+mv SDL3-3.1.5-Windows-VC/bin/SDL3.dll fnalibs/x64/
+mv FAudio-SDL3-x64/FAudio.dll fnalibs/x64/
+mv FNA3D-SDL3-x64/FNA3D.dll fnalibs/x64/
+mv Theorafile-x64/libtheorafile.dll fnalibs/x64/
