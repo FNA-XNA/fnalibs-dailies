eba59a02deca96066f68404699ab4267300269df
Ethan Lee <flibitijibibo@gmail.com>
2025-02-12 11:14:36 -0500

    Update for new artifact names

--- build-fnalibs-apple.sh
+++ build-fnalibs-apple.sh
@@ -10,14 +10,14 @@ gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-ios-arm64'
 gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-tvos-arm64'
 
 workflow_id=$(gh -R FNA-XNA/FAudio run list -b master --json databaseId --jq '.[0].databaseId')
-gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-osx'
-gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-iphoneos'
-gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-appletvos'
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-osx'
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-iphoneos'
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-appletvos'
 
 workflow_id=$(gh -R FNA-XNA/FNA3D run list -b master --json databaseId --jq '.[0].databaseId')
-gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-osx'
-gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-iphoneos'
-gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-appletvos'
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-osx'
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-iphoneos'
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-appletvos'
 
 workflow_id=$(gh -R FNA-XNA/Theorafile run list -b master --json databaseId --jq '.[0].databaseId')
 gh -R FNA-XNA/Theorafile run download ${workflow_id} -p 'Theorafile-osx'
@@ -32,23 +32,23 @@ hdiutil attach SDL-macos-framework/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VER
 cp /Volumes/SDL3\ 3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION/SDL3.framework/SDL3 fnalibs-apple/osx/libSDL3.0.dylib
 install_name_tool -id "@rpath/libSDL3.0.dylib" fnalibs-apple/osx/libSDL3.0.dylib
 hdiutil detach /Volumes/SDL3\ 3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION
-mv FAudio-SDL3-osx/libFAudio.0.dylib fnalibs-apple/osx/
-mv FNA3D-SDL3-osx/libFNA3D.0.dylib fnalibs-apple/osx/
+mv FAudio-osx/libFAudio.0.dylib fnalibs-apple/osx/
+mv FNA3D-osx/libFNA3D.0.dylib fnalibs-apple/osx/
 mv Theorafile-osx/libtheorafile.dylib fnalibs-apple/osx/
 xattr -c fnalibs-apple/osx/*.dylib
 
 mkdir -p fnalibs-apple/iphoneos
 tar xvfz SDL-ios-arm64/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-iOS.tar.gz SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-iOS/lib/libSDL3.a
 mv SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-iOS/lib/libSDL3.a fnalibs-apple/iphoneos/
-mv FAudio-SDL3-iphoneos/libFAudio.a fnalibs-apple/iphoneos/
-mv FNA3D-SDL3-iphoneos/libFNA3D.a fnalibs-apple/iphoneos/
-mv FNA3D-SDL3-iphoneos/libmojoshader.a fnalibs-apple/iphoneos/
+mv FAudio-iphoneos/libFAudio.a fnalibs-apple/iphoneos/
+mv FNA3D-iphoneos/libFNA3D.a fnalibs-apple/iphoneos/
+mv FNA3D-iphoneos/libmojoshader.a fnalibs-apple/iphoneos/
 mv Theorafile-iphoneos/libtheorafile.a fnalibs-apple/iphoneos/
 
 mkdir -p fnalibs-apple/appletvos
 tar xvfz SDL-tvos-arm64/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-tvOS.tar.gz SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-tvOS/lib/libSDL3.a
 mv SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-tvOS/lib/libSDL3.a fnalibs-apple/appletvos/
-mv FAudio-SDL3-appletvos/libFAudio.a fnalibs-apple/appletvos/
-mv FNA3D-SDL3-appletvos/libFNA3D.a fnalibs-apple/appletvos/
-mv FNA3D-SDL3-appletvos/libmojoshader.a fnalibs-apple/appletvos/
+mv FAudio-appletvos/libFAudio.a fnalibs-apple/appletvos/
+mv FNA3D-appletvos/libFNA3D.a fnalibs-apple/appletvos/
+mv FNA3D-appletvos/libmojoshader.a fnalibs-apple/appletvos/
 mv Theorafile-appletvos/libtheorafile.a fnalibs-apple/appletvos/
--- build-fnalibs.sh
+++ build-fnalibs.sh
@@ -10,14 +10,14 @@ gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-VC-x86'
 gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-VC-x64'
 
 workflow_id=$(gh -R FNA-XNA/FAudio run list -b master --json databaseId --jq '.[0].databaseId')
-gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-lib64'
-gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-x86'
-gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-SDL3-x64'
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-lib64'
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-x86'
+gh -R FNA-XNA/FAudio run download ${workflow_id} -p 'FAudio-x64'
 
 workflow_id=$(gh -R FNA-XNA/FNA3D run list -b master --json databaseId --jq '.[0].databaseId')
-gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-lib64'
-gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-x86'
-gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-SDL3-x64'
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-lib64'
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-x86'
+gh -R FNA-XNA/FNA3D run download ${workflow_id} -p 'FNA3D-x64'
 
 workflow_id=$(gh -R FNA-XNA/Theorafile run list -b master --json databaseId --jq '.[0].databaseId')
 gh -R FNA-XNA/Theorafile run download ${workflow_id} -p 'Theorafile-lib64'
@@ -32,20 +32,20 @@ tar xvfz SDL-slrsniper/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux.t
 mv SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION fnalibs/lib64/libSDL3.so.0
 chmod +x fnalibs/lib64/libSDL3.so.0
 strip -S fnalibs/lib64/libSDL3.so.0
-mv FAudio-SDL3-lib64/libFAudio.so.0 fnalibs/lib64/
-mv FNA3D-SDL3-lib64/libFNA3D.so.0 fnalibs/lib64/
+mv FAudio-lib64/libFAudio.so.0 fnalibs/lib64/
+mv FNA3D-lib64/libFNA3D.so.0 fnalibs/lib64/
 mv Theorafile-lib64/libtheorafile.so fnalibs/lib64/
 
 mkdir -p fnalibs/x86
 unzip SDL-VC-x86/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Windows-VC.zip SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Windows-VC/bin/SDL3.dll
 mv SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Windows-VC/bin/SDL3.dll fnalibs/x86/
-mv FAudio-SDL3-x86/FAudio.dll fnalibs/x86/
-mv FNA3D-SDL3-x86/FNA3D.dll fnalibs/x86/
+mv FAudio-x86/FAudio.dll fnalibs/x86/
+mv FNA3D-x86/FNA3D.dll fnalibs/x86/
 mv Theorafile-x86/libtheorafile.dll fnalibs/x86/
 
 mkdir -p fnalibs/x64
 unzip SDL-VC-x64/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Windows-VC.zip SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Windows-VC/bin/SDL3.dll
 mv SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Windows-VC/bin/SDL3.dll fnalibs/x64/
-mv FAudio-SDL3-x64/FAudio.dll fnalibs/x64/
-mv FNA3D-SDL3-x64/FNA3D.dll fnalibs/x64/
+mv FAudio-x64/FAudio.dll fnalibs/x64/
+mv FNA3D-x64/FNA3D.dll fnalibs/x64/
 mv Theorafile-x64/libtheorafile.dll fnalibs/x64/
