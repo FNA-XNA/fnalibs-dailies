dcc7727c3aee0f93be75bff974e49ec55f7d09d5
Ethan Lee <flibitijibibo@gmail.com>
2025-05-08 15:00:06 -0400

    Update build-fnalibs.sh

--- build-fnalibs.sh
+++ build-fnalibs.sh
@@ -43,8 +43,8 @@ mv FNA3D-lib64/libFNA3D.so.0 fnalibs/lib64/
 mv Theorafile-lib64/libtheorafile.so fnalibs/lib64/
 
 mkdir -p fnalibs/libaarch64
-tar xvfz SDL-steamrt3-arm64/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux.tar.gz SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION
-mv SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION fnalibs/libaarch64/libSDL3.so.0
+tar xvfz SDL-steamrt3-arm64/dist/SDL3-3.3.0-Linux.tar.gz SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION
+mv SDL3-3.3.0-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION fnalibs/libaarch64/libSDL3.so.0
 chmod +x fnalibs/libaarch64/libSDL3.so.0
 strip -S fnalibs/libaarch64/libSDL3.so.0
 mv FAudio-libaarch64/libFAudio.so.0 fnalibs/libaarch64/
