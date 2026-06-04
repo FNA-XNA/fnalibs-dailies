87f839c7851ac45257aec868f98b1d7fbbb2561c
Ethan Lee <flibitijibibo@gmail.com>
2026-06-03 20:42:23 -0400

    Self-build SDL3 for Steam Linux Runtime 3.0

--- build-fnalibs.sh
+++ build-fnalibs.sh
@@ -5,8 +5,6 @@ source VERSIONS
 set -ex
 
 workflow_id=$(gh -R libsdl-org/SDL run list -b release-3.4.x --json databaseId --jq '.[0].databaseId')
-gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-steamrt3'
-gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-steamrt3-arm64'
 gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-VC-x86'
 gh -R libsdl-org/SDL run download ${workflow_id} -p 'SDL-VC-x64'
 
@@ -32,19 +30,13 @@ mkdir -p fnalibs
 cp fnalibs.README fnalibs/README.txt
 
 mkdir -p fnalibs/lib64
-tar xvfz SDL-steamrt3/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux.tar.gz SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION
-mv SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION fnalibs/lib64/libSDL3.so.0
-chmod +x fnalibs/lib64/libSDL3.so.0
-strip -S fnalibs/lib64/libSDL3.so.0
+mv SDL3-steamrt3-x86_64/libSDL3.so.0 fnalibs/lib64/
 mv FAudio-lib64/libFAudio.so.0 fnalibs/lib64/
 mv FNA3D-lib64/libFNA3D.so.0 fnalibs/lib64/
 mv Theorafile-lib64/libtheorafile.so fnalibs/lib64/
 
 mkdir -p fnalibs/libaarch64
-tar xvfz SDL-steamrt3-arm64/dist/SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux.tar.gz SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION
-mv SDL3-3.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION-Linux/lib/libSDL3.so.0.$SDL_MINOR_VERSION.$SDL_PATCH_VERSION fnalibs/libaarch64/libSDL3.so.0
-chmod +x fnalibs/libaarch64/libSDL3.so.0
-aarch64-linux-gnu-strip -S fnalibs/libaarch64/libSDL3.so.0
+mv SDL3-steamrt3-aarch64/libSDL3.so.0 fnalibs/libaarch64/
 mv FAudio-libaarch64/libFAudio.so.0 fnalibs/libaarch64/
 mv FNA3D-libaarch64/libFNA3D.so.0 fnalibs/libaarch64/
 mv Theorafile-libaarch64/libtheorafile.so fnalibs/libaarch64/
